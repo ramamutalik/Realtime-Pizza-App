d450e823f185f112092a41621ee3d5e0096e6e4e
Rama Mutalikdesai <68442061+ramamutalik@users.noreply.github.com>
2022-07-22 13:14:58 +0530

    make cart page and auth

--- server.js
+++ server.js
@@ -7,9 +7,7 @@ const path = require('path')
 const expressLayout=require('express-ejs-layouts')
 const PORT = process.env.PORT || 3000
 
-app.get('/',(req,res)=>{
-    res.render('home')
-})
+
 
 //assets
 app.use(express.static('public'))
@@ -19,6 +17,21 @@ app.use(express.static('public'))
 app.use(expressLayout)
 app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs')
+
+app.get('/',(req,res)=>{
+    res.render('home')
+})
+app.get('/cart',(req,res)=>{
+    res.render('customers/cart')
+})
+app.get('/login',(req,res)=>{
+    res.render('auth/login')
+})
+app.get('/register',(req,res)=>{
+    res.render('auth/register')
+})
+
+
 
 
 app.listen(PORT,()=>{
