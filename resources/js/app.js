2cf82b07b2c647c8fdb45b130efcc7bced16c5be
Rama Mutalikdesai <68442061+ramamutalik@users.noreply.github.com>
2022-07-23 23:12:56 +0530

    make cart using db

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,36 @@
-console.log('Helli from app js')
+import axios from 'axios'
+import Noty from 'noty'
+
+let addToCart = document.querySelectorAll('.add-to-cart')
+let cartCounter = document.querySelector('#cartCounter')
+
+function updateCart(pizza){
+    //ajax call
+    axios.post('/update-cart',pizza).then(res=>{
+        console.log(res)
+        // cartCounter.innerText = res.data.tatalPrice
+        cartCounter.innerText = res.data.tatalQty
+        new Noty({
+            type: 'success',
+            timeout: 1000,
+            progressBar:false,
+            text:'Item added to cart'
+        }).show();
+    }).catch(err => {
+        new Noty({
+            type: 'error',
+            timeout: 1000,
+            progressBar:false,
+            text:'Something went wrong'
+        }).show();
+    })
+}
+
+addToCart.forEach((btn) => {
+    btn.addEventListener('click',(e)=>{
+        let pizza = JSON.parse(btn.dataset.pizza)
+        updateCart(pizza)
+        console.log(pizza)
+        
+    })
+})
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 
 const app = express()
@@ -6,31 +7,65 @@ const ejs=require('ejs')
 const path = require('path')
 const expressLayout=require('express-ejs-layouts')
 const PORT = process.env.PORT || 3000
+const mongoose=require('mongoose')
+const session = require('express-session')
+const flash = require('express-flash')
+const MongoDbStore=require('connect-mongo')
 
+
+//Database connection
+const url = 'mongodb://localhost:27017/pizza';
+
+// Use connect method to connect to the server
+mongoose.connect(url,      err => {
+    if(err) throw err;
+    console.log('connected to MongoDB')
+});
+
+const connection=mongoose.connection;
+connection.once('open',()=>{
+    console.log('Database connected...');
+})
+.on('error' ,()=> {
+    console.log('Connection failed....');
+});
+
+//session store
+ let mongoStore= MongoDbStore.create({
+    mongoUrl: url,
+    collection : 'sessions'
+})
+
+//session config
+app.use(session({
+    secret : process.env.COOKIE_SECRET,
+    resave: false,
+    store:mongoStore,
+    saveUninitialized:false,
+    cookie:{maxAge : 1000 * 60*60*24}//24HOUR
+}))
+
+
+app.use(flash())
+
+//global middleware
+app.use((req,res,next)=>{
+    res.locals.session=req.session
+    next()
+})
 
 
 //assets
 app.use(express.static('public'))
-
+app.use(express.json())
 
 //set Template engine
 app.use(expressLayout)
 app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs')
 
-app.get('/',(req,res)=>{
-    res.render('home')
-})
-app.get('/cart',(req,res)=>{
-    res.render('customers/cart')
-})
-app.get('/login',(req,res)=>{
-    res.render('auth/login')
-})
-app.get('/register',(req,res)=>{
-    res.render('auth/register')
-})
-
+//routes
+require('./routes/web')(app)
 
 
 
